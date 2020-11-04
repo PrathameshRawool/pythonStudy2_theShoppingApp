2accb243d6b8e63103cecf131e94b3f6a81fd870
Prathamesh Rawool <prathameshvijay.rawool20@gmail.com>
2020-11-04 14:03:22 +0530

    Create script.py

---
+++ page3/script.py
@@ -0,0 +1,15 @@
+fruits = ['apple', 'banana', 'orange']
+
+# Add 'grape' to 'fruits'
+fruits.append('grape')
+
+# Print 'fruits'
+print(fruits)
+
+# Update the element at index 0
+fruits[0] = 'cherry'
+
+# Print the element at index 0
+print(fruits[0])
+
+
